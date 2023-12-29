6ab765730a23db62e10f86f50f58188f6adb0dbb
Jefino 1537 <jefinojacob1537@gmail.com>
2023-12-30 00:29:04 +0530

    test

---
+++ scripts/install_linux_aitools.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# SPDX-FileCopyrightText: 2020 Intel Corporation
+#
+# SPDX-License-Identifier: MIT
+
+URL=$1
+
+curl --output webimage.sh --url "$URL" --retry 5 --retry-delay 5
+chmod +x webimage.sh
+sudo ./webimage.sh -b -p /intel/oneapi/intelpython -k
+installer_exit_code=$?
+exit $installer_exit_code
